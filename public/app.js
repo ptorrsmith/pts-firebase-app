dec8f852c2c69fcea14dd0870330cb043d70b53c
Peter <peter@ptorrsmith.com>
2020-10-06 22:41:38 +1300

    initialize auth elements and events

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
-console.log("Now LOGGING: ", firebase)
+// Vanilla JS  - ES4 / ES5 ?  hence the semi-colons ;
+
+// console.log("Now LOGGING: ", firebase)
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);  // return a promise
+
+signOutBtn.onclick = () => auth.signOut();  // return a promise
+// debugger
